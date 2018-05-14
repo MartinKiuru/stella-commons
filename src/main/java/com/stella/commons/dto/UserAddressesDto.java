ad08924dab854a4e0450c78d538ebd14282c7db3
Jan Tumanov <jan.tumanov@ignite.ee>
2018-05-14 11:00:37 +0300

    UserAddressesDto

---
+++ src/main/java/com/stella/commons/dto/UserAddressesDto.java
@@ -0,0 +1,23 @@
+package com.stella.commons.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAddressesDto implements Serializable {
+
+    private String stellaAddress;
+    private String stellaPublicKey;
+    private String bitcoinAddress;
+    private String ethereumAddress;
+    private String litecoinAddress;
+    private String rippleAddress;
+    private String userEmail;
+}
