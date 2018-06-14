5ccdcfd00f6c66649610910b29568a543e0722de
Jan Tumanov <jan.tumanov@ignite.ee>
2018-06-14 12:34:03 +0300

    userDto to commons

---
+++ src/main/java/com/stella/commons/dto/UserDto.java
@@ -0,0 +1,42 @@
+package com.stella.commons.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    private String companyName;
+
+    @NotNull
+    private LocalDate birthDate;
+
+    private String password;
+
+    private String stellaWalletAddress;
+    private String stellaWalletPublicKey;
+    private String bitcoinAddress;
+    private String ethereumAddress;
+    private String litecoinAddress;
+    private String rippleAddress;
+    private String resetToken;
+}
